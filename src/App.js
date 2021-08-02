71cd65d6ae71e0d0b6a3d58ac23e1f655ce88a93
RITIK KAPOOR <61417822+rkapoor10@users.noreply.github.com>
2021-08-02 21:37:37 +0530

    update app.js added footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import cake from "/src/cake.svg";
 export default function App() {
   const [alertBox, setAlertBox] = useState("flex");
   const [dobInput, setDobInput] = useState([]);
+  const [mainSection, setMainSection] = useState("block");
   const [luckyNoInput, setLuckyNoInput] = useState(0);
   const [output, setOutput] = useState("");
 
@@ -34,10 +35,11 @@ export default function App() {
     if (dobArray[0] >= 9999) {
       setOutput(
         <>
-          <div>Enter a valid date of Birth </div>
+          <div className="box">Enter a valid date of Birth </div>
         </>
       );
     } else {
+      setMainSection("none");
       let sum = 0;
       dobArray.map((element) => {
         sum = sum + Number(element);
@@ -63,8 +65,11 @@ export default function App() {
               <Lottie options={notLuckyOptions} height={200} width={200} />
             </div>
             <div style={{ fontSize: "1.2rem" }}>Not it's Not</div>
-            <div className="message">
-              But Luck is what happens when preparation meets opportunity!
+            <div style={{ fontSize: "1.2rem" }}>
+              <span className="message">
+                But Luck is what happens when preparation meets opportunity!
+              </span>
+              <br /> So, trust your preparation 😊.
             </div>
           </>
         );
@@ -80,7 +85,15 @@ export default function App() {
         </h1>
         <img alt="cake" src={cake} width="100%" height="200px" />
 
-        <a className="clickScroll" href="#main">
+        <a
+          onClick={() => {
+            setMainSection("block");
+            setAlertBox("flex");
+            setOutput("");
+          }}
+          className="clickScroll"
+          href="#main"
+        >
           {" "}
           click to check{" "}
         </a>
@@ -88,49 +101,102 @@ export default function App() {
 
       {/* scroll down feature to be added */}
       <div id="main" className="container">
-        <h2>Enter the details </h2>
-        <div className="box" style={{ display: `${alertBox}` }}>
-          <strong>Privacy &nbsp;</strong> We are not storing your DOB
-          <div
-            onClick={() => {
-              setAlertBox("none");
-            }}
-            style={{
-              cursor: "pointer",
-              paddingLeft: "1.7rem",
-              fontSize: "0.7rem"
-            }}
-          >
-            <span role="img" aria-labelledby="crossIcon">
-              ❌
-            </span>
+        <div style={{ display: `${mainSection}` }}>
+          <h2>Enter the details </h2>
+          <div className="box" style={{ display: `${alertBox}` }}>
+            <strong>Privacy &nbsp;</strong> We are not storing your DOB
+            <div
+              onClick={() => {
+                setAlertBox("none");
+              }}
+              style={{
+                cursor: "pointer",
+                paddingLeft: "1.7rem",
+                fontSize: "0.7rem"
+              }}
+            >
+              <span role="img" aria-labelledby="crossIcon">
+                ❌
+              </span>
+            </div>
           </div>
+
+          <label>Date of Birth</label>
+          <input
+            className="inputBox"
+            type="date"
+            required
+            onChange={(event) => {
+              setDobInput(event.target.value);
+            }}
+          ></input>
+
+          <label>Your Lucky Number</label>
+          <input
+            className="inputBox"
+            type="Number"
+            min="1"
+            step="1"
+            required
+            onChange={(event) => {
+              setLuckyNoInput(event.target.value);
+            }}
+          ></input>
+
+          <button onClick={onClickHandler}>Check</button>
         </div>
-
-        <label>Date of Birth</label>
-        <input
-          className="inputBox"
-          type="date"
-          required
-          onChange={(event) => {
-            setDobInput(event.target.value);
-          }}
-        ></input>
-
-        <label>Your Lucky Number</label>
-        <input
-          className="inputBox"
-          type="Number"
-          min="1"
-          step="1"
-          required
-          onChange={(event) => {
-            setLuckyNoInput(event.target.value);
-          }}
-        ></input>
-
-        <button onClick={onClickHandler}>Check</button>
         <div>{output}</div>
+
+        <footer>
+          <hr />
+          <ul>
+            <li>
+              <a
+                target="_blank"
+                href="https://github.com/rkapoor10/lucky-birthday-reactApp"
+              >
+                Github |{" "}
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/ritik-kapoor-493469196/"
+                target="blank"
+              >
+                Linkedin |{" "}
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/dev.kapsk/?hl=en"
+                target="blank"
+              >
+                Instagram |{" "}
+              </a>
+            </li>
+            <li>
+              <a href="https://ritikportfolio.netlify.app/" target="blank">
+                Portfolio{" "}
+              </a>
+            </li>
+          </ul>
+          <div>
+            {" "}
+            © 2021 | dev.kapsk |{" "}
+            <u
+              style={{
+                cursor: "pointer"
+              }}
+              onClick={() => {
+                setAlertBox("flex");
+              }}
+            >
+              Privacy Policy
+            </u>
+          </div>
+        </footer>
+
+        {/* */}
       </div>
     </div>
   );
